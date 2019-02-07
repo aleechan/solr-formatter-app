7d30c07a90213dffbfa5c191e4faf08b751e88b7
aleechan <firefly1585@gmail.com>
2019-02-06 23:07:01 -0500

    Implemented basic functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Formatter extends Component {
+  constructor() {
+    super();
+    this.state = { text: 'Test', formattedQuery: '' }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    let query = event.target.value;
+    let str = query.replace(/[\n\r\t]/gm, '');
+    let i = 0;
+    let depth = 0;
+    let result = '';
+    while (i < str.length){
+     
+      if(str.charAt(i) === '('){
+        result =result.concat(str.charAt(i))
+        depth++;
+        result = result.concat("\n")
+        for(var j  =0; j < depth; j++){
+          result = result.concat("\t")
+        }
+      }else if (str.charAt(i) === ')'){
+        depth--;
+        result = result.concat("\n")
+        for(var j  =0; j < depth; j++){
+          result = result.concat("\t")
+        }
+        result =result.concat(str.charAt(i))
+        result = result.concat("\n")
+        for(var j  =0; j < depth; j++){
+          result = result.concat("\t")
+        }
+      }else{
+        result =result.concat(str.charAt(i))
+      }
+      i++;
+    }
+    
+    this.setState({ text: query, formattedQuery: result});
+  }
+  render() {
+    return (
+      <div>
+        <textarea value={this.state.text} onChange={this.handleChange} />
+        <br />
+        <textarea value={this.state.formattedQuery} />
+      </div>
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Solr Query Formatter</h1>
         </header>
-      </div>
-    );
+        <Formatter />
+      </div>);
   }
 }
 
