f1eb47a1630d912c18d49cf49f79f1eafc272185
aleechan <firefly1585@gmail.com>
2019-02-18 14:11:21 -0500

    Added semantic ui and basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
 import formatter from './formatter.js';
+import { Grid, TextArea, Header } from 'semantic-ui-react';
 
 class Formatter extends Component {
   constructor() {
     super();
-    this.state = { text: '', formattedQuery: '' };
-
+    this.state = { text: '', formattedQuery: '', minQuery:''};
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
@@ -14,32 +13,44 @@ class Formatter extends Component {
     //compress spaces not in quotes
     var str = formatter.compactWhiteSpace(query);
     var result = formatter.parseQuery(str);
-    this.setState({ text: query, formattedQuery: result });
+    this.setState({ text: query, formattedQuery: result ,minQuery:str});
   }
+
   render() {
     return (
-      <div>
-        Query:
-        <br />
-        <textarea className="Formatter" value={this.state.text} onChange={this.handleChange} />
-        <br />
-        Formatted:
-        <br />
-        <textarea className="Formatter" readOnly value={this.state.formattedQuery} />
-      </div>
+      <Grid columns={2} divided style={{height: '95vh'}}>
+        <Grid.Row style={{height: "90%" }}>
+          <Grid.Column textAlign='center'>
+            <Header size='medium'>Query:</Header>
+            <TextArea style={{ width: "95%", minHeight: "95%" }} 
+                      value={this.state.text} 
+                      onChange={this.handleChange} 
+                      />
+          </Grid.Column>
+          <Grid.Column textAlign='center'>
+            <Header size='medium'>Formatted:</Header>
+            <TextArea style={{ width: "95%", minHeight: "85%"  }}
+                      value={this.state.formattedQuery} 
+                      />
+            <Header size='medium'>Compact:</Header>
+            <TextArea style={{ width: "95%", minHeight: "5%"  }}
+                      value={this.state.minQuery} 
+                      />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Solr Query Formatter</h1>
-        </header>
+      <div>
+        <Header size='huge' dividing>
+          Solr Query Formatter
+        </Header>
         <Formatter />
       </div>);
   }
 }
-
 export default App;
