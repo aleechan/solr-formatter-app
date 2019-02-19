2aa105b37ce7cd683f0311098be3a8014aed74da
aleechan <firefly1585@gmail.com>
2019-02-18 22:10:38 -0500

    Added menubar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import formatter from './formatter.js';
-import { Grid, TextArea, Header, Form, Radio, Button, Divider, Segment } from 'semantic-ui-react';
+import { Grid, TextArea, Header, Form, Radio, Button, Divider, Segment, Menu } from 'semantic-ui-react';
 
 class Formatter extends Component {
   constructor() {
@@ -35,20 +35,18 @@ class Formatter extends Component {
       <Grid columns={3} style={{ height: '95vh' }}>
         <Grid.Row style={{ height: "90%" }}>
           <Grid.Column textAlign='center' width={7}>
-            <Header size='medium'>Query:</Header>
+            <Header size='medium' content='Query:'/>
             <TextArea style={{ width: "95%", minHeight: "100%", resize: "none" }}
               value={this.state.text}
               onChange={this.handleQueryChange}
             />
-
           </Grid.Column>
           <Grid.Column stretched textAlign='center' verticalAlign='middle' width={2} height={100}>
             <Grid.Row>
               <Segment>
-         
                 <Form onSubmit={this.updateSettings}>
                   <Form.Field>
-                    <Header size='medium'>Indent Style:</Header>
+                    <Header size='medium' content='Indent Style:'/>
                   </Form.Field>
                   <Form.Field>
                     <Radio label='Tabs' name='indentStyle' value='tabs' checked={this.state.indent === 'tabs'} onChange={this.updateSettings} />
@@ -60,16 +58,14 @@ class Formatter extends Component {
                 <Divider />
                 <Button content='Copy' icon='left arrow' onClick={() => { this.updateQuery(this.state.formattedQuery) }} />
               </Segment>
-
             </Grid.Row>
-
           </Grid.Column>
           <Grid.Column textAlign='center' width={7}>
             <Header size='medium'>Formatted:</Header>
             <TextArea style={{ width: "95%", minHeight: "85%", resize: "none" }}
               value={this.state.formattedQuery}
             />
-            <Header size='medium'>Compact:</Header>
+            <Header size='medium' content='Compact:'/>
             <TextArea style={{ width: "95%", minHeight: "10%", resize: "none" }}
               value={this.state.minQuery}
             />
@@ -83,9 +79,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header size='huge' dividing>
-          Solr Query Formatter
-        </Header>
+        <Menu>
+          <Menu.Item header>
+            <Header size='huge' content= 'Solr Query Formatter'/>
+          </Menu.Item>
+        </Menu>
+
         <Formatter />
       </div>);
   }
