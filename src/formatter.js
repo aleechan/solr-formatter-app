954f6ad745f2b9322ae7d5d8020e71fe08f0668d
aleechan <firefly1585@gmail.com>
2019-02-18 22:32:13 -0500

    Minor formatting fix.

--- src/formatter.js
+++ src/formatter.js
@@ -8,7 +8,7 @@ const formatter = ({
         str = str.replace(/(?<=\))\s*(?=\))/gm, '');
         return str;
     },
-    parseQuery(str,indentStr,maxLength) {
+    parseQuery(str, indentStr, maxLength) {
         var i = 0;
         var depth = 0;
         var result = '';
@@ -18,12 +18,12 @@ const formatter = ({
                 part = part.trim();
                 //if the last character was '(' ignoring whitespace, the part being printed has content,  this not the first open bracket in a block and the last character printed was not a ":"
                 if (result.match(/\([\s]*$/gm) && part.length > 0 && depth > 0 && !part.match(/:$/)) {
-                    result += this.nextLine(depth,indentStr);
+                    result += this.nextLine(depth, indentStr);
                 }
                 result += part.trim();
                 //if part does not contain + or - or :
                 if (!part.match(/[+-:]/gm)) {
-                    result += this.nextLine(depth,indentStr);
+                    result += this.nextLine(depth, indentStr);
                 }
                 depth++;
                 part = '';
@@ -35,19 +35,19 @@ const formatter = ({
                     var indent = result.match(/[^\s]*$/g)[0].length;
                     result += parts[0].trim();
                     for (var j = 1; j < parts.length; j++) {
-                        result += this.nextLine(depth-2,indentStr);
+                        result += this.nextLine(depth - 2, indentStr);
                         result += ' '.repeat(indent);
                         result += parts[j].trim();
                     }
-                    result += this.nextLine(depth-1,indentStr);
-                    result +=  str.charAt(i);
+                    result += this.nextLine(depth - 1, indentStr);
+                    result += str.charAt(i);
                     depth--;
                     part = '';
                 } else {
                     //if the last character printed was ) start a new line indented 1 less than the current depth
                     //Also skip this if the last set to brackets was defining the field value
                     if ((result.charAt(result.length - 1) === ')' && result.charAt(result.lastIndexOf('(') - 1) !== ':') || result.match(/\)\)$/g) || result.lastIndexOf('\n') > result.lastIndexOf('(')) {
-                        result += this.nextLine(depth - 1,indentStr);
+                        result += this.nextLine(depth - 1, indentStr);
                     }
                     result += part.trim();
                     depth--;
@@ -66,7 +66,7 @@ const formatter = ({
         result += part;
         return result;
     },
-    nextLine(depth,indentStr) {
+    nextLine(depth, indentStr) {
         var str = '\n';
         if (depth > 0) str += indentStr.repeat(depth);
         return str;
@@ -88,14 +88,16 @@ const formatter = ({
             if (char === '"') {
                 inQuotes = !inQuotes;
             }
-            if ("+- ".indexOf(char) > -1 ) {
+            if ("+- ".indexOf(char) > -1) {
                 canSplit = false;
             } else {
                 canSplit = true;
             }
         }
+        if (part.trim().length !== 0) {
+            parts.push(part);
+        }
 
-        parts.push(part);
         return parts;
     }
 });
